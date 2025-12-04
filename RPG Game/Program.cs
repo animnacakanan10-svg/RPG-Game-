73d883ec813db106969d6e05e9e95692b2f3790b
yehorzvoznikov-hash <yehorzvoznikov@gmail.com>
2025-12-04 13:00:06 +0200

    Make Character script

--- RPG Game/Program.cs
+++ RPG Game/Program.cs
@@ -2,14 +2,7 @@
 
 namespace RPG_Game
 {
-    public abstract class Character
-    {
-        private int _health;
-        private int _maxHealth;
-        private int _strength;
-        private string _name;
-    }
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
