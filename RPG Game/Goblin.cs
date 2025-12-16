be551bb10b7c3a86829cb76d47908ceb0a8f28f9
yehorzvoznikov-hash <yehorzvoznikov@gmail.com>
2025-12-16 12:09:48 +0200

    Add Dark Mage class

---
+++ RPG Game/Goblin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RPG_Game
+{
+    public class Goblin : Enemy
+    {
+        public Goblin() : base(40, 8, "Гоблін", 30)
+        {
+            
+        }
+
+        public override void Attack(Character target)
+        {
+            var damage = Strength;
+            Console.WriteLine($"{Name} б'є кинджалом і завдає {damage} пощкоджень!");
+            target.TakeDamage(damage);
+        }
+    }
+}
